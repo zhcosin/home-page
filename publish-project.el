0bccccf2c66347870cf98ca4ba137499b0fe820c
zhcosin <zhcosin@gmail.com>
2018-01-05 16:31:56 +0800

    modify the path.

--- publish-project.el
+++ publish-project.el
@@ -3,17 +3,17 @@
 (setq org-publish-project-alist
       '(
          ("org-files"
-          :base-directory "~/zhcosin-home-page/org/"
+          :base-directory "./org/"
           :base-extension "org"
-          :publishing-directory "~/zhcosin-home-page/html/"
+          :publishing-directory "./html/"
           :recursive t
           :publishing-function org-html-publish-to-html
           :headline-levels 4
           :auto-preamble t)   
          ("resource-files"
-          :base-directory "~/zhcosin-home-page/org/"
+          :base-directory "./org/"
           :base-extension "css\\|js\\|png\\|jpg\\|jpeg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-          :publishing-directory "~/zhcosin-home-page/html/"
+          :publishing-directory "./html/"
           :recursive t
           :publishing-function org-publish-attachment) 
          ("home-page"
